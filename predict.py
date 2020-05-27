30b4ff850b9f2aea32e26607ea80a463ee45079a
Ashutosh Sanzgiri <sanzgiri@gmail.com>
2020-05-26 21:53:33 -0700

    test predict

--- predict.py
+++ predict.py
@@ -3,59 +3,66 @@ import numerapi
 import os
 import model
 
-tournaments = nx.tournament_names()
-print(tournaments)
+def predict():
+
+    tournaments = nx.tournament_names()
+    print(tournaments)
 
 
-# download dataset from numerai
-data = nx.download('numerai_dataset.zip')
+    # download dataset from numerai
+    data = nx.download('numerai_dataset.zip')
 
-for tournament_name in tournaments:
-    saved_model_name = 'model_trained_' + tournament_name
-    if os.path.exists(saved_model_name):
-        print("using saved model for", tournament_name)
-        m = model.LinearModel.load(saved_model_name)
-    else:
-        print("saved model not found for", tournament_name)
-        m = model.LinearModel(verbose=True)
+    return
 
-        print("training model for", tournament_name)
-        m.fit(data['train'], tournament_name)
+    for tournament_name in tournaments:
+        saved_model_name = 'model_trained_' + tournament_name
+        if os.path.exists(saved_model_name):
+            print("using saved model for", tournament_name)
+            m = model.LinearModel.load(saved_model_name)
+        else:
+            print("saved model not found for", tournament_name)
+            m = model.LinearModel(verbose=True)
 
-    print("running predictions for", tournament_name, flush=True)
-    # fit model with train data and make predictions for tournament data
-    prediction = nx.production(m, data, tournament=tournament_name)
+            print("training model for", tournament_name)
+            m.fit(data['train'], tournament_name)
 
-    # save predictions to csv file
-    prediction_filename = '/tmp/prediction_' + tournament_name + '.csv'
-    prediction.to_csv(prediction_filename, verbose=True)
+        print("running predictions for", tournament_name, flush=True)
+        # fit model with train data and make predictions for tournament data
+        prediction = nx.production(m, data, tournament=tournament_name)
 
-# submit the prediction
+        # save predictions to csv file
+        prediction_filename = '/tmp/prediction_' + tournament_name + '.csv'
+        prediction.to_csv(prediction_filename, verbose=True)
 
-# Numerai API key
-# You will need to create an API key by going to https://numer.ai/account and clicking "Add" under the "Your API keys" section.
-# Select the following permissions for the key: "Upload submissions", "Make stakes", "View historical submission info", "View user info"
-public_id = os.environ["NUMERAI_PUBLIC_ID"]
-secret_key = os.environ["NUMERAI_SECRET_KEY"]
+    # submit the prediction
 
-for tournament_name in tournaments:
-    prediction_filename = '/tmp/prediction_' + tournament_name + '.csv'
+    # Numerai API key
+    # You will need to create an API key by going to https://numer.ai/account and clicking "Add" under the "Your API keys" section.
+    # Select the following permissions for the key: "Upload submissions", "Make stakes", "View historical submission info", "View user info"
+    public_id = os.environ["NUMERAI_PUBLIC_ID"]
+    secret_key = os.environ["NUMERAI_SECRET_KEY"]
 
-    submission_id = nx.upload(
-        prediction_filename,
-        tournament_name,
-        public_id,
-        secret_key,
-        block=False,
-        n_tries=3,
-        model_id=model.model_id)
+    for tournament_name in tournaments:
+        prediction_filename = '/tmp/prediction_' + tournament_name + '.csv'
 
-# staking variables
-# change block in nx.upload to block=True. This is because you can't stake until the submission has finished its checks, which take a few minutes
-# confidence = .501 # increase this number to signify your confidence in a minimum AUC. Can't go below .501
-# stake_value = .1 # the amount of NMR to stake
+        submission_id = nx.upload(
+            prediction_filename,
+            tournament_name,
+            public_id,
+            secret_key,
+            block=False,
+            n_tries=3,
+            model_id=model.model_id)
 
-# napi = numerapi.NumerAPI(public_id, secret_key)
+    # staking variables
+    # change block in nx.upload to block=True. This is because you can't stake until the submission has finished its checks, which take a few minutes
+    # confidence = .501 # increase this number to signify your confidence in a minimum AUC. Can't go below .501
+    # stake_value = .1 # the amount of NMR to stake
 
-# for tournament_name in tournaments:
-#   napi.stake(confidence, stake_value, nx.tournament_int(tournament_name))
+    # napi = numerapi.NumerAPI(public_id, secret_key)
+
+    # for tournament_name in tournaments:
+    #   napi.stake(confidence, stake_value, nx.tournament_int(tournament_name))
+
+if __name__ == "__main__":
+    predict()
